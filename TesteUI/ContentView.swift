385019515611424cd64d21150065e2fe675f36f6
Bruno Alves <alves.bruno945@gmail.com>
2020-08-09 22:54:54 -0300

    SwiftUI Animation
    A simple animation using SwiftUI

---
+++ TesteUI/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  TesteUI
+//
+//  Created by Bruno Silva on 09/08/20.
+//  Copyright © 2020 Bruno Silva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var backViewSize : CGFloat = 80
+    @State var size : CGSize = .zero
+    
+    var body: some View {
+        
+        GeometryReader{reader in
+            
+            ZStack {
+                
+                VStack {
+
+                    Color.purple
+                    
+                }.frame(width: reader.size.width - 160, height: 300)
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                    .offset(y: -30)
+                
+                VStack {
+                    
+                    Color.green
+                    
+                }.frame(width: reader.size.width - self.backViewSize, height: 300)
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                    .offset(y: -15)
+                
+                VStack {
+                    
+                    Color.red
+                    
+                }.frame(width: reader.size.width - 50, height: 300)
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                    .gesture(DragGesture().onChanged({ (value) in
+                        
+                        self.size = value.translation
+                        self.backViewSize = 50
+                    })
+                        .onEnded({ (value) in
+                            self.size = .zero
+                            self.backViewSize = 80
+                        }))
+                    .offset(self.size)
+            }
+        }.animation(.spring())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
